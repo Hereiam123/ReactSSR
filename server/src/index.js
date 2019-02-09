b53d8662c97e7016f40d5645e3afffc6fff91f8d
hereiam123 <bdemaio1@gmail.com>
2019-02-09 08:22:40 -0500

    Setting up React in express server

---
+++ server/src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const React = require("react");
+const renderToString = require("react-dom/server").renderToString;
+const Home = require("./client/components/Home").default;
+
+app.get("/", (req, res) => {});
+
+app.listen(3000, () => {
+  console.log("Listening on 3000");
+});
