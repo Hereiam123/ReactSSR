b8d349a1d5a6d7d170e4f8fc99f879e4ccda9324
hereiam123 <bdemaio1@gmail.com>
2019-02-09 08:31:40 -0500

    Setting up webpack config for server

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,11 @@ const React = require("react");
 const renderToString = require("react-dom/server").renderToString;
 const Home = require("./client/components/Home").default;
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  const content = renderToString(<Home />);
+
+  res.send(content);
+});
 
 app.listen(3000, () => {
   console.log("Listening on 3000");
