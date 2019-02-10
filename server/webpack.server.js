1e17f0f31b5a3edb5760f91c1032480444f479da
hereiam123 <bdemaio1@gmail.com>
2019-02-10 01:51:12 -0500

    Webpack cleanup

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const baseConfig = require("./webpack.base.js");
+const webpackNodeExternals = require("webpack-node-externals");
 
 const config = {
   //Bulding bundle for nodeJS
@@ -9,7 +10,8 @@ const config = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build")
-  }
+  },
+  externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
