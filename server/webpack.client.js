dd051e5f71dde609d538daf646b3c973fa6bbd7e
hereiam123 <bdemaio1@gmail.com>
2019-02-10 01:40:04 -0500

    Using webpack-merge to merge repeated code

--- server/webpack.client.js
+++ server/webpack.client.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const merge = require("webpack-merge");
+const baseConfig = require("./webpack.base.js");
 
 module.exports = {
   //Bulding bundle for client
@@ -6,26 +8,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            "react",
-            "stage-0",
-            [
-              "env",
-              {
-                targets: { browsers: ["last 2 versions"] }
-              }
-            ]
-          ]
-        }
-      }
-    ]
   }
 };
+
+module.exports = merge(baseConfig, config);
