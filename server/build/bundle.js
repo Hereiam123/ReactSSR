ee76cfa4710114c2b634650c80f22ea65d861d8b
hereiam123 <bdemaio1@gmail.com>
2019-02-10 17:56:25 -0500

    Loading initial state from server into client

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -256,7 +256,7 @@ exports.default = function (req, store) {
       )
     )
   ));
-  return "\n    <html>\n        <head></head>\n        <body>\n            <div id='root'>" + content + "</div>\n            <script src='bundle.js'></script>\n        </body>\n    </html>\n  ";
+  return "\n    <html>\n        <head></head>\n        <body>\n            <div id='root'>" + content + "</div>\n            <script>window.INITIAL_STATE=" + JSON.stringify(store.getState()) + "</script>\n            <script src='bundle.js'></script>\n        </body>\n    </html>\n  ";
 };
 
 /***/ }),
