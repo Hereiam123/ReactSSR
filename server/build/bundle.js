333cbddee9b474b067be7c34a8350551ce03acf4
hereiam123 <bdemaio1@gmail.com>
2019-02-10 19:01:58 -0500

    Setting up proxy in server for future authentication browser requests

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -126,6 +126,10 @@ var _express2 = _interopRequireDefault(_express);
 
 var _reactRouterConfig = __webpack_require__(0);
 
+var _expressHttpProxy = __webpack_require__(21);
+
+var _expressHttpProxy2 = _interopRequireDefault(_expressHttpProxy);
+
 var _Routes = __webpack_require__(1);
 
 var _Routes2 = _interopRequireDefault(_Routes);
@@ -142,6 +146,13 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var app = (0, _express2.default)();
 
+//Options specific to this api, Google Oauth flow
+app.use("/api", (0, _expressHttpProxy2.default)("http://react-srr-api.herokuapp.com", {
+  proxyReqOptDecorator: function proxyReqOptDecorator(opts) {
+    opts.header["x-forwarded-host"] = "localhost:3000";
+    return opts;
+  }
+}));
 app.use(_express2.default.static("public"));
 app.get("*", function (req, res) {
   var store = (0, _createStore2.default)();
@@ -529,6 +540,12 @@ module.exports = require("axios");
 /***/ (function(module, exports) {
 
 module.exports = require("serialize-javascript");
+
+/***/ }),
+/* 21 */
+/***/ (function(module, exports) {
+
+module.exports = require("express-http-proxy");
 
 /***/ })
 /******/ ]);
--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,23 @@
 import "babel-polyfill";
 import express from "express";
 import { matchRoutes } from "react-router-config";
+import proxy from "express-http-proxy";
 import Routes from "./client/Routes";
 import renderer from "./helpers/renderer";
 import createStore from "./helpers/createStore";
 
 const app = express();
 
+//Options specific to this api, Google Oauth flow
+app.use(
+  "/api",
+  proxy("http://react-srr-api.herokuapp.com", {
+    proxyReqOptDecorator(opts) {
+      opts.header["x-forwarded-host"] = "localhost:3000";
+      return opts;
+    }
+  })
+);
 app.use(express.static("public"));
 app.get("*", (req, res) => {
   const store = createStore();
